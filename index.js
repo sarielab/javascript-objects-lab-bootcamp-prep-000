9fba46f4b2fed0e177ac32e09d2cf7f01c17b4fd
Poppy Puspa Sari <poppy.puspasari@yahoo.co.id>
2019-07-14 16:07:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,55 +2,14 @@ const updateObjectWithKeyAndValue = (object, key, value) => {
   return Object.assign({}, object, { [key]: value })
 }
 
+const destructivelyUpdateObjectWithKeyAndValue = (object, key, value) => {
+  return Object.assign(object, { [key]: value })
+}
 
-/*
+const deleteFromObjectByKey = (object, key) => {
+  return Object.assign({}, object, { [key]: null } )
+}
 
-  describe('destructivelyUpdateObjectWithKeyAndValue(object, key, value)', function() {
-    it('updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object', function() {
-      var obj = { prop: 1 }
-
-      expect(destructivelyUpdateObjectWithKeyAndValue(obj, 'prop2', 2)).toMatch({
-        prop: 1,
-        prop2: 2
-      })
-
-      expect(obj).toMatch({
-        prop: 1,
-        prop2: 2
-      })
-    })
-  })
-
-  describe('deleteFromObjectByKey(object, key)', function() {
-    it('deletes `key` from a clone of object and returns the new object (it is non-destructive)', function() {
-      var obj = { prop: 1 }
-      var newObj = deleteFromObjectByKey(obj, 'prop')
-
-      expect(newObj['prop']).toBe(undefined)
-    })
-
-    it('does not modify the original object (it is non-destructive)', function() {
-      var obj = { prop: 1 }
-
-      deleteFromObjectByKey(obj, 'prop')
-      expect(obj['prop']).toBe(1)
-    })
-  })
-
-  describe('destructivelyDeleteFromObjectByKey(object, key)', function() {
-    it('returns object without the delete key/value pair', function() {
-      var obj = { prop: 1 }
-      var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
-
-      expect(newObj['prop']).toBe(undefined)
-    })
-
-    it('modifies the original object', function() {
-      var obj = { prop: 1 }
-      var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
-
-      expect(obj['prop']).toBe(undefined)
-    })
-  })
-
-*/
+const destructivelyDeleteFromObjectByKey = (object, key) => {
+  return delete object[key]
+}
